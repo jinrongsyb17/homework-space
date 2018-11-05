9a035f7b5160300bf8a9fce2ffd60de7b62e0959
iamwaterlee <43404045+iamwaterlee@users.noreply.github.com>
2018-11-05 19:08:19 +0800

    第三次-2017312297-李姗珊-金融实验

---
+++ 李姗珊/Homework6_19.cpp
@@ -0,0 +1,23 @@
+//
+//  Homework6_19.cpp
+//  Test1
+//
+//  Created by 李姗珊 on 2018/11/3.
+//  Copyright © 2018年 李姗珊. All rights reserved.
+//
+
+#include "Homework6_19.hpp"
+
+long (*function)(int a){
+    return static_cast<long>(a);
+}
+class A{
+    
+public:
+    A();
+    long *fuction(int a){
+    }
+    
+   
+}
+
