f6d13fa60dacdd694bc94eb959dec6e49e4c2339
Dede593 <851182239@qq.com>
2018-11-06 19:30:34 +0800

    第3次-2017311802-刘闻笛-金融实验
    第3次-2017311802-刘闻笛-金融实验

---
+++ 刘闻笛/第3次-2017311802-刘闻笛-金融实验/6-21.cpp
@@ -0,0 +1,27 @@
+// 6-21.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+int count(string str) {
+	int i, num = 0;
+	for (i = 0; str[i]; i++) {
+		if ((str[i]>='a'&&str[i] <= 'z') || (str[i] >= 'A'&&str[i] <= 'Z'))
+			num++;
+	}
+	return num;
+}
+
+void main()
+{
+	string text;
+	cout << "输入一个英语句子:" << endl;
+	getline(cin, text);
+
+	cout << "这个句子里有" << count(text) << "个字母" << endl;
+	return ;
+}
+
