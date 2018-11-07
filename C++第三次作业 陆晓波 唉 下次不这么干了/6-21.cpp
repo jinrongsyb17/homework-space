19fbdf9c4df40f9d346b656cf21eb32d190f6fc6
13531109752 <43348796+13531109752@users.noreply.github.com>
2018-11-07 13:03:08 +0800

    Add files via upload

---
+++ C++第三次作业 陆晓波 唉 下次不这么干了/6-21.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+ int count (char*str)
+{
+	int i, s = 0;
+	for (i = 0;str[i];i++)
+	{
+		if ((str[i] >= 'a'&&str[i] <= 'z') || (str[i] >= 'A'&&str[i] <= 'Z'))
+			s++;
+	}
+	return s;
+}
+ int main()
+{
+	char text[100];
+	cout << "请输入一个英文句子" << endl;
+	gets(text);
+	cout << "这个句子里的字母数为" << count(text) << endl;
+}
