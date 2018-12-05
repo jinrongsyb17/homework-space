e55203c4fd8ed8fa035c2c067031783866cfd697
Groundsel1009 <44011710+Groundsel1009@users.noreply.github.com>
2018-12-05 12:41:16 +0800

    第5次-2017310140-李涵-金融实验

---
+++ 李涵/第5次-2017310140-李涵-金融实验/7-11.cpp
@@ -0,0 +1,41 @@
+// ConsoleApplication3.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class BaseClass {
+public:
+	void fn1();
+	void fn2();
+};
+void BaseClass::fn1() {
+	cout << "调用基类函数fn1()" << endl;
+}
+void BaseClass::fn2() {
+	cout << "调用基类函数fn2()" << endl;
+}
+class DerivedClass :public BaseClass {
+public:
+	void fn1();
+	void fn2();
+};
+void DerivedClass::fn1() {
+	cout << "调用派生类函数fn1()" << endl;
+}
+void DerivedClass::fn2() {
+	cout << "调用派生类函数fn2()" << endl;
+}
+int main()
+{
+	DerivedClass aDerivedClass;
+	DerivedClass *pDerivedClass = &aDerivedClass;
+	BaseClass *pBaseclass = &aDerivedClass;
+	aDerivedClass.fn1();
+	aDerivedClass.fn2();
+	pBaseclass->fn1();
+	pBaseclass->fn2();
+	pDerivedClass->fn1();
+	pDerivedClass->fn2();
+    return 0;
+}
+
