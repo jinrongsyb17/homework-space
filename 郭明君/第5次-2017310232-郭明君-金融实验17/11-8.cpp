62eb99a96cf04b783c1ef5d2686a1abafbed3d8d
Staquel <44015895+Staquel@users.noreply.github.com>
2018-12-05 12:35:17 +0800

    Add files via upload

---
+++ 郭明君/第5次-2017310232-郭明君-金融实验17/11-8.cpp
@@ -0,0 +1,16 @@
+// 11-8.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "请输入一个十进制整数：";
+	cin >> n;
+	cout << "这个数的十进制形式为：" << dec << n << endl;
+	cout << "这个数的八进制形式为：" << oct << n << endl;
+	cout << "这个数的十六进制形式为：" << hex << n << endl;
+	return 0;
+}
