3dc0664004d1b465fa5e0008111e12998823af2e
13531109752 <43348796+13531109752@users.noreply.github.com>
2018-12-19 23:48:17 +0800

    马克思主义强大的生命力根源在于其以实践为基础的科学性与革命性的统一

---
+++ 陆晓波/第七次C++作业/11-6(文本方式).cpp
@@ -0,0 +1,25 @@
+#include <iostream> //preprocessor directives
+#include <fstream>
+#include <string>
+using namespace std ;
+//using namespace std;
+//using namespace 使用命名空间
+//std : standard library 标准程序库
+class Dog{
+public:
+int weight , age ;
+Dog(int x , int y):weight(x) , age(y){} ;
+Dog(){} ;
+};
+int main(){
+Dog dog1(5 , 10) ;
+ofstream file("dog.txt" , ios_base::out|ios_base::binary) ;
+file.write(reinterpret_cast<char*>(&dog1) , sizeof(dog1)) ;
+Dog dog2 ;
+file.close() ;
+ifstream File("dog.txt" , ios_base::in|ios_base::binary) ;
+File.read(reinterpret_cast<char*>(&dog2) , sizeof(dog2)) ;
+File.close() ;
+return 0 ;
+}
+
