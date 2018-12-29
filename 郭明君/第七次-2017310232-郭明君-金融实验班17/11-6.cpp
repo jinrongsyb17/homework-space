3d47172cd879398e10f2a8b8012a5cdb63ccf278
Staquel <44015895+Staquel@users.noreply.github.com>
2018-12-29 14:36:08 +0800

    Add files via upload

---
+++ 郭明君/第七次-2017310232-郭明君-金融实验班17/11-6.cpp
@@ -0,0 +1,42 @@
+// 11-6.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Dog {
+public:
+	Dog(int w, int a) :weight(w), age(a) {}
+	~Dog() {}
+	int getWeight()const {
+		return weight;
+	}
+	int getAge()const {
+		return age;
+	}
+private:
+	int weight, age;
+};
+
+int main() {
+	char fileName[80];
+	cout << "Please enter the file name:";
+	cin >> fileName;
+	ofstream fout(fileName);
+
+	Dog dog1(5, 10);
+	fout.write((char*)&dog1, sizeof dog1);
+	fout.close();
+
+	ifstream fin(fileName);
+
+	Dog dog2(1, 1);
+	fin.read((char*)&dog2, sizeof dog2);
+	cout << dog2.getWeight() << endl;
+	cout << dog2.getAge() << endl;
+	fin.close();
+	return 0;
+}
+
